4329067c0a97fa76b2f82bbb5a9c9e60e0da952b
WEI ZHENG <wei.zheng@cootek.cn>
2015-03-30 10:26:22 +0800

    add sql scripts and some widget classes

--- testTornado/simpleDemo/hello_world.py
+++ testTornado/simpleDemo/hello_world.py
@@ -1,8 +1,14 @@
+import torndb
 import tornado.ioloop
 import tornado.web
+import tornado.gen
+import tornado.httpclient
+
+database = torndb.Connection('localhost', 'share_theme', user='root', password='')
 
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
+		print(database.execute('show tables'))
 		self.write("Hello world!!")
 
 class StoryHandler(tornado.web.RequestHandler):
@@ -20,10 +26,21 @@ class MyFormHandler(tornado.web.RequestHandler):
 		self.set_header("Content-Type", "text/plain")
 		self.write("You wrote " + self.get_argument("message"))
 
+class GetFullPageAsyncHandler(tornado.web.RequestHandler):
+	@tornado.gen.coroutine
+	def get(self):
+		http_client = tornado.httpclient.AsyncHTTPClient()
+		http_response = yield http_client.fetch("http://www.drdobbs.com/web-development")
+		response = http_response.body.decode().replace("Most Recent Premium Content", "Most Recent Content")
+		self.write(response)
+		self.set_header("Content-Type", "text/html")
+
+
 application = tornado.web.Application([
-	(r"/", MyFormHandler),		
+	(r"/", MainHandler),		
 	(r"/myform", MyFormHandler),
-	("r/story/([0-9]+)", StoryHandler),
+	(r"/story/([0-9]+)", StoryHandler),
+	(r"/getFullPage", GetFullPageAsyncHandler),
 	(r"/static/tornado-0.2.tar.gz", tornado.web.RedirectHandler,
 	 dict(url = "https://github.com/downloads/facebook/tornado/tornado-0.2.tar.gz")),
 ])
